a6987deb668b3f5742f068e85ba848fc4c29ae32
jchrist <jchrist@cytech.gr>
2016-03-27 22:22:38 +0300

    * Added BranchUpdater, which    - Checks out the local branch to update    - Merges it with the tracked remote    - Keeps list of updated files to be displayed: Still experimental, cannot work since IJ computes differences using the file path, which will not be correct for changes happening in the not-currently-checked-out branch * refactored git extender to use branch updater for updating a local branch from its remote

--- src/gr/jchrist/gitextender/GitExtenderUpdateAll.java
+++ src/gr/jchrist/gitextender/GitExtenderUpdateAll.java
@@ -1,20 +1,25 @@
 package gr.jchrist.gitextender;
 
+import com.intellij.dvcs.DvcsUtil;
 import com.intellij.dvcs.repo.VcsRepositoryManager;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.AccessToken;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Key;
+import com.intellij.openapi.vcs.VcsException;
+import com.intellij.vcsUtil.VcsImplUtil;
 import git4idea.GitPlatformFacade;
-import git4idea.GitRemoteBranch;
 import git4idea.GitUtil;
-import git4idea.commands.*;
+import git4idea.commands.Git;
+import git4idea.commands.GitCommand;
+import git4idea.commands.GitCommandResult;
+import git4idea.commands.GitLineHandler;
 import git4idea.repo.GitBranchTrackInfo;
 import git4idea.repo.GitRepository;
 import git4idea.repo.GitRepositoryManager;
@@ -68,19 +73,26 @@ public class GitExtenderUpdateAll extends AnAction {
                 return;
             }
 
-            updateRepositories(repositoryList);
+            updateRepositories(project, repositoryList);
         } catch (Exception | Error e) {
-            showErrorNotification("Git Extender Update Failed", "Gi Extender failed to update the project due to exception: " + e);
+            showErrorNotification("Git Extender Update Failed", "Git Extender failed to update the project due to exception: " + e);
         }
     }
 
-    private void updateRepositories(@NotNull List<GitRepository> repositories) {
+    private void updateRepositories(@NotNull Project project, @NotNull List<GitRepository> repositories) {
+        //get an access token for changing the repositories
+        final AccessToken accessToken = DvcsUtil.workingTreeChangeStarted(project);
+
         final AtomicInteger countDown = new AtomicInteger(repositories.size());
+
         for (final GitRepository repo : repositories) {
-            new Task.Backgroundable(repo.getProject(), "Updating " + repo.getPresentableUrl(), false) {
+            final String repoName = VcsImplUtil.getShortVcsRootName(repo.getProject(), repo.getRoot());
+            new Task.Backgroundable(repo.getProject(), "Updating " + repoName, false) {
                 public void run(@NotNull ProgressIndicator indicator) {
-                    updateRepository(repo, indicator);
+                    updateRepository(repo, indicator, repoName);
                     if (countDown.decrementAndGet() <= 0) {
+                        //the last task finished should clean up, release project changes and show info notification
+                        DvcsUtil.workingTreeChangeFinished(repo.getProject(), accessToken);
                         showInfoNotification("Update Completed", "Git Extender updated all projects");
                     }
                 }
@@ -88,48 +100,52 @@ public class GitExtenderUpdateAll extends AnAction {
         }
     }
 
-    private void updateRepository(@NotNull GitRepository repo, @NotNull ProgressIndicator indicator) {
+    private void updateRepository(
+            @NotNull final GitRepository repo,
+            @NotNull ProgressIndicator indicator,
+            @NotNull final String repoName
+    ) {
         //check if repo is valid for updating
-        if (!canRepoBeUpdated(repo)) {
+        if (!canRepoBeUpdated(repo, repoName)) {
             return;
         }
 
-        //find git service
-        Git git = ServiceManager.getService(repo.getProject(), Git.class);
+        final Project project = repo.getProject();
 
-        String currBranch = repo.getCurrentBranchName();
-        GitCommandResult result;
+        //find git service
+        Git git = ServiceManager.getService(project, Git.class);
+
+        final String currBranch = repo.getCurrentBranchName();
 
         //check if we require stashing
-        boolean stagedChanges;
-        boolean unstagedChanges;
+        boolean repoChanges;
         try {
-            stagedChanges = GitUtil.hasLocalChanges(true, repo.getProject(), repo.getRoot());
-            unstagedChanges = GitUtil.hasLocalChanges(false, repo.getProject(), repo.getRoot());
+            repoChanges = hasChanges(repo);
         } catch (Exception e) {
             showErrorNotification("Git Extender update failed",
-                    "Git repo: " + repo.getPresentableUrl() + "<br>" +
+                    "Git repo: " + repoName + "<br>" +
                             "Error: Git Extender failed to update git repo, " +
                             "because it could not identify if there were any staged/unstaged changes. " +
                             "The exception was: " + e.getMessage());
             return;
         }
 
-        if (stagedChanges || unstagedChanges) {
+        if (repoChanges) {
             //there are changes that we need to stash
-            result = git.stashSave(repo, "GitExtender_Stashing");
-            if (!result.success()) {
+            GitCommandResult saveResult = git.stashSave(repo, "GitExtender_Stashing");
+            if (!saveResult.success()) {
                 showErrorNotification("Git Extender failed to stash changes",
-                        "Git repo: " + repo.getPresentableUrl() + "<br>" +
-                                "Error: " + result.getErrorOutputAsJoinedString());
+                        "Git repo: " + repoName + "<br>" +
+                                "Error: " + saveResult.getErrorOutputAsJoinedString());
                 return;
             }
         }
 
         try {
+
             //fetch and prune remote
             //git fetch origin
-            boolean fetchResult = new GitFetcher(repo.getProject(), indicator, true)
+            boolean fetchResult = new GitFetcher(project, indicator, true)
                     .fetchRootsAndNotify(Collections.singleton(repo), null, true);
 
             if (!fetchResult) {
@@ -140,41 +156,34 @@ public class GitExtenderUpdateAll extends AnAction {
             //update the repo after fetch, in order to re-sync locals and remotes
             repo.update();
 
-            for (GitBranchTrackInfo info : repo.getBranchTrackInfos()) {
-                GitRemoteBranch remoteBranch = info.getRemoteBranch();
-                //checkout branch
-                GitCommand checkout = GitCommand.CHECKOUT;
-                GitLineHandler checkoutHandler = new GitLineHandler(repo.getProject(), repo.getRoot(), checkout);
-                checkoutHandler.addParameters(info.getLocalBranch().getName());
-                result = git.runCommand(checkoutHandler);
-                if (!result.success()) {
-                    showErrorNotification("Git Extender failed to checkout branch",
-                            "Local branch: " + info.getLocalBranch().getName() + "<br>" +
-                                    "Git repo:" + repo.getPresentableUrl() + "<br>" +
-                                    "Error: " + result.getErrorOutputAsJoinedString());
-                    continue;
-                }
+            for (final GitBranchTrackInfo info : repo.getBranchTrackInfos()) {
+                final BranchUpdater updater = new BranchUpdater(git, repo, repoName, info);
 
-                //do NOT rebase from remote after all :) just merge with -ff only
-                GitCommand merge = GitCommand.MERGE;
-                GitLineHandler mergeHandler = new GitLineHandler(repo.getProject(), repo.getRoot(), merge);
-                mergeHandler.addParameters("--ff-only");
-                mergeHandler.addParameters(remoteBranch.getNameForLocalOperations());
-                result = git.runCommand(mergeHandler);
-                if (!result.success()) {
-                    showErrorNotification("Git Extender failed to merge (with fast-forward only) branch",
-                            "Local branch: " + info.getLocalBranch().getName() + "<br>" +
-                                    "Remote branch: " + remoteBranch.getNameForLocalOperations() + "<br>" +
-                                    "Git repo:" + repo.getPresentableUrl() + "<br>" +
-                                    "Please perform the merge (which needs conflict resolution) manually for this branch, " +
-                                    "by checking it out, merging the changes and resolving the conflicts");
-                    //merge failed, nothing was done, just carry on to other branches
+                BranchUpdater.BranchUpdateResult result = updater.update();
+
+                //let's see if we had any errors
+                if (!result.isSuccess()) {
+                    //where was the error?
+                    //in checkout ?
+                    if (result.wasCheckoutError()) {
+                        showErrorNotification("Git Extender failed to checkout branch",
+                                "Local branch: " + updater.getLocalBranchName() + "<br>" +
+                                        "Git repo:" + repoName + "<br>" +
+                                        "Error: " + result.checkoutResult.getErrorOutputAsJoinedString());
+                    } else if (result.wasMergeError()) {
+                        showErrorNotification("Git Extender failed to merge (with fast-forward only) branch",
+                                "Local branch: " + updater.getLocalBranchName() + "<br>" +
+                                        "Remote branch: " + updater.getRemoteBranchName() + "<br>" +
+                                        "Git repo:" + repoName + "<br>" +
+                                        "Please perform the merge (which may need conflict resolution) manually for this branch, " +
+                                        "by checking it out, merging the changes and resolving any conflicts");
+                    }
                 }
             }
 
             //now that we're finished, checkout again the branch that the developer had before
             GitCommand checkout = GitCommand.CHECKOUT;
-            GitLineHandler checkoutHandler = new GitLineHandler(repo.getProject(), repo.getRoot(), checkout);
+            GitLineHandler checkoutHandler = new GitLineHandler(project, repo.getRoot(), checkout);
             checkoutHandler.addParameters(currBranch);
             git.runCommand(checkoutHandler);
 
@@ -182,23 +191,13 @@ public class GitExtenderUpdateAll extends AnAction {
             repo.update();
         } finally {
             //now unstash, if we had stashed any changes
-            if (stagedChanges || unstagedChanges) {
-                git.stashPop(repo, new GitLineHandlerListener() {
-                    public void onLineAvailable(String line, Key outputType) {
-                    }
-
-                    public void processTerminated(int exitCode) {
-                    }
-
-                    public void startFailed(Throwable exception) {
-                    }
-                });
+            if (repoChanges) {
+                git.stashPop(repo);
             }
         }
     }
 
-    private boolean canRepoBeUpdated(@NotNull GitRepository repo) {
-        String repoName = repo.getPresentableUrl();
+    private boolean canRepoBeUpdated(@NotNull GitRepository repo, String repoName) {
         if (repo.isRebaseInProgress()) {
             showErrorNotification("Repository cannot be updated",
                     "Repository " + repoName + " cannot be updated, because there is a rebase in progress");
@@ -212,5 +211,10 @@ public class GitExtenderUpdateAll extends AnAction {
         }
 
         return true;
+    }
+
+    private boolean hasChanges(GitRepository repo) throws VcsException {
+        return GitUtil.hasLocalChanges(true, repo.getProject(), repo.getRoot()) ||
+                GitUtil.hasLocalChanges(false, repo.getProject(), repo.getRoot());
     }
 }
