4ce54d73d22bb65da0e80fee66f2ba4b8f1c29e8
jchrist <jchrist@cytech.gr>
2017-01-17 00:02:44 +0200

    added TestingUtil, containing hideous code to use LightPlatformCodeInsightFixtureTestCase in junit 4, without gradle switching me to junit 3

---
+++ src/test/java/gr/jchrist/gitextender/TestingUtil.java
@@ -0,0 +1,72 @@
+package gr.jchrist.gitextender;
+
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import git4idea.commands.GitCommandResult;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+
+public abstract class TestingUtil {
+    public static final GitCommandResult error =
+            new GitCommandResult(false, 1,
+                    Collections.singletonList("test error output"),
+                    Collections.singletonList("error"),
+                    new Exception("test exception"));
+    public static final GitCommandResult success =
+            new GitCommandResult(true, 0,
+                    Collections.emptyList(),
+                    Collections.singletonList("success"),
+                    null);
+
+    public static BaseTest getBaseTest() {
+        return new BaseTest() {
+        };
+    }
+
+    public static abstract class BaseTest {
+        private final Inner inner;
+
+        public BaseTest() {
+            inner = new Inner() {
+            };
+        }
+
+        public void setUp() throws Exception {
+            inner.setUp();
+        }
+
+        public void tearDown() throws Exception {
+            inner.tearDown();
+        }
+
+        public CodeInsightTestFixture getFixture() {
+            return inner.getFixture();
+        }
+
+        public void invokeTestRunnable(@NotNull Runnable runnable) throws Exception {
+            inner.invokeTestRunnable(runnable);
+        }
+
+        private static abstract class Inner extends LightPlatformCodeInsightFixtureTestCase {
+            @Override
+            public void setUp() throws Exception {
+                super.setUp();
+            }
+
+            @Override
+            public void tearDown() throws Exception {
+                super.tearDown();
+            }
+
+            public CodeInsightTestFixture getFixture() {
+                return myFixture;
+            }
+
+            @Override
+            public void invokeTestRunnable(@NotNull Runnable runnable) throws Exception {
+                super.invokeTestRunnable(runnable);
+            }
+        }
+    }
+}
