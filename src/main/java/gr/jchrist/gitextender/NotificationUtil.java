a1a9e2b7cf2c1a6b12142a90dc5b37aaf64b7b89
jchrist <io.christod@gmail.com>
2017-01-18 09:13:24 +0200

    moved notifications to a util class

---
+++ src/main/java/gr/jchrist/gitextender/NotificationUtil.java
@@ -0,0 +1,22 @@
+package gr.jchrist.gitextender;
+
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import org.jetbrains.annotations.NotNull;
+
+public class NotificationUtil {
+    public static final String NOTIFICATION_GROUP_ID = "Git Extender";
+
+    public static void showErrorNotification(@NotNull String title, @NotNull String content) {
+        showNotification(title, content, NotificationType.ERROR);
+    }
+
+    public static void showInfoNotification(@NotNull String title, @NotNull String content) {
+        showNotification(title, content, NotificationType.INFORMATION);
+    }
+
+    public static void showNotification(@NotNull String title, @NotNull String content, @NotNull NotificationType type) {
+        Notifications.Bus.notify(new Notification(NOTIFICATION_GROUP_ID, title, content, type));
+    }
+}
